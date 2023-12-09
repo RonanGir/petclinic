61f8a8b49176a440fc6eeabe0763c7723ffd565c
Ronan Girault <girault.ronan@gmail.com>
2023-12-09 17:01:37 +0100

    Add indexes and controllers for pets, vets and owner

---
+++ pet-clinic-web/src/main/java/spring/guru/udemy/petclinic/controllers/PetController.java
@@ -0,0 +1,16 @@
+package spring.guru.udemy.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/pets"})
+public class PetController {
+
+
+
+    @RequestMapping({"", "index", "index.html"})
+    public String listPets() {
+        return "/pets/index";
+    }
+}
---
+++ pet-clinic-web/src/main/java/spring/guru/udemy/petclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package spring.guru.udemy.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/vets"})
+public class VetController {
+
+
+    @RequestMapping({"", "index", "index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
